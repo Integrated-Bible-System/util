91f701e31d49a74130563b6c40df7a85d5269496
Masaru Kitajima <thirstytraveler@mac.com>
2021-03-06 17:28:55 +0900

    Prepared clean HTML template with Milligram, then changed the extention to php.

---
+++ WordsResoueces/lexicon/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Modufy lexicons</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS Framework Milligram-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <!-- My Own FontAwesome Pro-->
+    <script src="https://kit.fontawesome.com/223cc2ea20.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+</body>
+
+</html>
