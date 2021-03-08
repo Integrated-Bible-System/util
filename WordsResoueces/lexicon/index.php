9136c5379d965d4f2256592bc35c6e172e1562b6
Masaru Kitajima <thirstytraveler@mac.com>
2021-03-08 16:59:41 +0900

    何故かPHPでMacのディレクトリをis_dirでディレクトリとして認識しない為、やむを得ずサーバーで確認する事にする。

--- WordsResoueces/lexicon/index.php
+++ WordsResoueces/lexicon/index.php
@@ -46,7 +46,18 @@
             if ($handle = opendir($tgtDir)) {
                 $tgtArray = array();
                 while (false !== ($entry = readdir($handle))) {
-                    if ($entry != "." && $entry != "..") {
+                    if (is_dir($entry)) {
+                        echo($entry . ' is a directory<br>');
+                    }
+                    if ($entry === 'csv') {
+                        $chkSpec = $tgtDir .
+                        $resultdir = is_dir($entry) ? 'true' : 'false';
+                        $resultfile = is_file($entry) ? 'true' : 'false';
+                        echo('is_dir is ' . $resultdir . '<br>');
+                        echo('is_file is ' . $resultfile . '<br>');
+                    }
+                    if (is_file($entry)) {
+                        echo('Added file ' . $entry . '<br>');
                         array_push($tgtArray, $entry);
                     }
                 }
@@ -56,27 +67,36 @@
         }
         foreach ($files as $key => $ary) {
             $joinedJsonArray[$key] = [];
-            $csv[$key] = '';
             foreach ($ary as $file) {
+                echo('file from arry = <br>');
+                var_dump($file);
+                echo('<br><br>');
                 $tgtFile = $cwd . '/' . $key . '/' . $file;
+                $path_parts = pathinfo($tgtFile);
+                echo('tgtFile = ' . $tgtFile . '<br>');
+                echo('path info is<br>');
+                var_dump($path_parts);
+                echo('<hr>');
+                $process_file = $path_parts['filename'];
+                echo('<h2>Processing ' . $process_file . '</h2>');
                 $contents = file_get_contents($tgtFile);
                 $json = mb_convert_encoding($contents, 'UTF8');
                 $jsonarr = json_decode($json, true);
                 $joinedJsonArray[$key] += $jsonarr;
-                var_dump($jsonarr);
-                echo('<br>');
+                $outfile_fullpath = $path_parts['dirname'] . '/csv/' . $path_parts['basename'] . '.csv';
                 /*
-                $filename = $cwd . '/' . $key . '.csv';
-                $fp = fopen($filename, 'w');
-                $line = array();
+                $fp = fopen($outfile_fullpath, 'w');
                 foreach ($joinedJsonArray[$key] as $jkey => $jary) {
-                    foreach ($jary as $ckey => $cval) {
-                        $line[] = $cval;
+                    $line = array();
+                    foreach ($jary as $ikey => $ival) {
+                        $line[] = $ival;
                     }
                     fputcsv($fp, $line);
                 }
                 fclose($fp);
                 */
+                $outfilename = $path_parts['basename'] . '.csv';
+                echo('<p>File ' . $outfilename . ' was created</p>');
             }
         }
         echo("finished");
