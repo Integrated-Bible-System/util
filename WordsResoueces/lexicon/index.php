732cc3e60f6adbf5496ec373d411553f72851a2d
Masaru Kitajima <thirstytraveler@mac.com>
2021-03-08 18:25:49 +0900

    Succeeded to create csv files, though during json decoding process, enclosure signs are missed. Need to fix it. Also, Added symple styles.

--- WordsResoueces/lexicon/index.php
+++ WordsResoueces/lexicon/index.php
@@ -10,97 +10,94 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <!-- Override by my own -->
+    <link rel="stylesheet" href="css/style.css">
     <!-- My Own FontAwesome Pro-->
     <script src="https://kit.fontawesome.com/223cc2ea20.js" crossorigin="anonymous"></script>
 
 </head>
 
 <body>
-    <div>
-        <h1>Modify lexicons JSON files</h1>
-    </div>
-    <div>
-        <?php
-        $cwd = getcwd();
-        ?>
-        <h2>本アプリ実行PATH</h2>
-        <p><?php echo($cwd);?></p>
-        <?php
-        $targets = array();
-        $tagetDirs = array();
-        $files = scandir($cwd);
-        foreach ($files as $val) {
-            if ($val !== '.' && $val !== '..' && is_dir($val)) {
-                array_push($targets, $val);
-            }
-        }
-        ?>
-    </div>
-    <div>
-        <?php
-        $files = [];
-        $joinedJsonArray = [];
-        $csv = [];
-        foreach ($targets as $tgt) {
-            $tgtDir = $cwd . '/' . $tgt;
-            if ($handle = opendir($tgtDir)) {
-                $tgtArray = array();
-                while (false !== ($entry = readdir($handle))) {
-                    if (is_dir($entry)) {
-                        echo($entry . ' is a directory<br>');
-                    }
-                    if ($entry === 'csv') {
-                        $chkSpec = $tgtDir .
-                        $resultdir = is_dir($entry) ? 'true' : 'false';
-                        $resultfile = is_file($entry) ? 'true' : 'false';
-                        echo('is_dir is ' . $resultdir . '<br>');
-                        echo('is_file is ' . $resultfile . '<br>');
-                    }
-                    if (is_file($entry)) {
-                        echo('Added file ' . $entry . '<br>');
-                        array_push($tgtArray, $entry);
+    <div class="container">
+        <div class="row">
+            <div class="column column-100">
+                <h1>Modify lexicons JSON files</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="column column-80 column-center">
+                <?php
+                $cwd = getcwd();
+                ?>
+                <h2>本アプリ実行PATH</h2>
+                <span><?php echo($cwd);?></span>
+                <?php
+                $targets = array();
+                $tagetDirs = array();
+                $files = scandir($cwd);
+                foreach ($files as $val) {
+                    if ($val !== '.' && $val !== '..' && is_dir($val) && $val !== 'css') {
+                        array_push($targets, $val);
                     }
                 }
-                $files[$tgt] = $tgtArray;
-            }
-            asort($files[$tgt], SORT_NUMERIC);
-        }
-        foreach ($files as $key => $ary) {
-            $joinedJsonArray[$key] = [];
-            foreach ($ary as $file) {
-                echo('file from arry = <br>');
-                var_dump($file);
-                echo('<br><br>');
-                $tgtFile = $cwd . '/' . $key . '/' . $file;
-                $path_parts = pathinfo($tgtFile);
-                echo('tgtFile = ' . $tgtFile . '<br>');
-                echo('path info is<br>');
-                var_dump($path_parts);
-                echo('<hr>');
-                $process_file = $path_parts['filename'];
-                echo('<h2>Processing ' . $process_file . '</h2>');
-                $contents = file_get_contents($tgtFile);
-                $json = mb_convert_encoding($contents, 'UTF8');
-                $jsonarr = json_decode($json, true);
-                $joinedJsonArray[$key] += $jsonarr;
-                $outfile_fullpath = $path_parts['dirname'] . '/csv/' . $path_parts['basename'] . '.csv';
-                /*
-                $fp = fopen($outfile_fullpath, 'w');
-                foreach ($joinedJsonArray[$key] as $jkey => $jary) {
-                    $line = array();
-                    foreach ($jary as $ikey => $ival) {
-                        $line[] = $ival;
+                ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="column column-70 column-center">
+                <?php
+                $files = [];
+                $joinedJsonArray = [];
+                $csv = [];
+                foreach ($targets as $tgt) {
+                    clearstatcache();
+                    $tgtDir = $cwd . '/' . $tgt;
+                    if ($handle = opendir($tgtDir)) {
+                        $tgtArray = array();
+                        while (false !== ($entry = readdir($handle))) {
+                            if (!is_dir($tgt . '/' . $entry)) {
+                                array_push($tgtArray, $entry);
+                            }
+                        }
+                        $files[$tgt] = $tgtArray;
                     }
-                    fputcsv($fp, $line);
+                    asort($files[$tgt], SORT_NUMERIC);
                 }
-                fclose($fp);
-                */
-                $outfilename = $path_parts['basename'] . '.csv';
-                echo('<p>File ' . $outfilename . ' was created</p>');
-            }
-        }
-        echo("finished");
-        ?>
+                foreach ($files as $key => $ary) {
+                    $joinedJsonArray[$key] = [];
+                    foreach ($ary as $file) {
+                        $tgtFile = $cwd . '/' . $key . '/' . $file;
+                        $path_parts = pathinfo($tgtFile);
+                        $process_file = $path_parts['filename'];
+                        echo('<h4>Processing ' . $process_file . '</h4>');
+                        $contents = file_get_contents($tgtFile);
+                        $json = mb_convert_encoding($contents, 'UTF8');
+                        $jsonarr = json_decode($json, true);
+                        $joinedJsonArray[$key] += $jsonarr;
+                        $outfile_fullpath = $path_parts['dirname'] . '/csv/' . $path_parts['filename'] . '.csv';
+                        $fp = fopen($outfile_fullpath, 'w');
+                        foreach ($joinedJsonArray[$key] as $jkey => $jary) {
+                            $line = array();
+                            foreach ($jary as $ikey => $ival) {
+                                $line[] = $ival;
+                            }
+                            fputcsv($fp, $line, ',', '"', "\\");
+                        }
+                        fclose($fp);
+                        $outfilename = $path_parts['filename'] . '.csv';
+                        echo('<p>File ' . $outfilename . ' was created</p>');
+                    }
+                }
+                ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="column column-80 column-center">
+                <?php
+                echo('<h2>Finished!</h2>');
+                ?>
+            </div>
+        </div>
     </div>
 </body>
 
